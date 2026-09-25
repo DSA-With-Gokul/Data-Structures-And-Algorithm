467c1e05d7ddd22638aace209165f0d46d305cc2
DSA-With-Gokul <gokuldsa024@gmail.com>
2026-09-25 15:19:30 +0530

    Time: 24 ms (28.25%), Space: 92.9 MB (78.97%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> s = new HashSet<>();
+        for(int i : nums){
+            s.add(i);
+        }
+        if(s.size()==nums.length){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
